24804113c64adea0f7bda21b9bcda1ba1cc9be9f
MusaShojen <musashojen@gmail.com>
2024-01-07 22:05:24 +0300

    Initial commit

---
+++ Balance Eat/Balance_EatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Balance_EatApp.swift
+//  Balance Eat
+//
+//  Created by Муса Шогенов on 7.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Balance_EatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Balance Eat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Balance Eat
+//
+//  Created by Муса Шогенов on 7.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
