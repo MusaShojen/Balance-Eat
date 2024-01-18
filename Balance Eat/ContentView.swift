41d123ce80d678675bc57e1baa68c6bed9fd9a19
MusaShojen <musashojen@gmail.com>
2024-01-18 13:23:34 +0300

    Tabbar *needs to be changed

--- Balance Eat/ContentView.swift
+++ Balance Eat/ContentView.swift
@@ -9,17 +9,112 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    init(){
+        UITabBar.appearance().isHidden = true
+    }
+    
+    @State var selected = "1"
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, Balance eat")
+        //  Spacer()
+        ZStack(alignment: .bottom){
+            
+            TabView(selection: $selected,
+                    content:  {
+                HomePageView().tabItem { Text("fdfd") }.tag("1")
+                
+                
+                Text("Tab Content 2")
+                
+                    .tabItem { Text("Tab Label 2") }.tag("2")
+                
+                Text("Tab Content 3").tabItem { Text("Tab Label 3") }.tag("3")
+                
+                Text("Tab Content 4")
+                
+                    .tabItem { Text("Tab Label 4") }.tag("4")
+                
+                
+                
+                
+            })
+            
+            Spacer()
+            HStack(spacing: 47){
+                
+                Button {
+                    selected = "1"
+                    
+                } label: {
+                    Image(systemName: "heart.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundStyle(selected == "1" ? .blue : .gray)
+                        .frame(width: 28, height: 28, alignment: .center)
+                }
+                
+                Button {
+                    selected = "2"
+                    
+                } label: {
+                    Image(systemName: "fork.knife")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundStyle(selected == "2" ? .blue : .gray)
+                        .frame(width: 28, height: 28, alignment: .center)
+                }
+                
+                Button {
+                    
+                    
+                } label: {
+                    Image(systemName: "plus.circle.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundStyle(.gray)
+                        .frame(width: 28, height: 28, alignment: .center)
+                }
+                
+                Button {
+                    
+                    selected = "3"
+                    
+                } label: {
+                    Image(systemName: "chart.bar.xaxis")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundStyle(selected == "3" ? .blue : .gray)
+                        .frame(width: 28, height: 28, alignment: .center)
+                }
+                
+                Button {
+                    selected = "4"
+                    
+                } label: {
+                    Image(systemName: "person.2.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundStyle(selected == "4" ? .blue : .gray)
+                        .frame(width: 28, height: 28, alignment: .center)
+                }
+                
+            }
+            
+            .padding(.vertical, 20)
+            .padding(.horizontal, 30)
+            .frame(maxWidth: .infinity)
+            .background(.white)
+            //  .clipShape(RoundedRectangle(cornerRadius: 10))
+            
+            .edgesIgnoringSafeArea(.bottom)
+            
+            
+            
             
         }
-        .padding()
     }
 }
+
 
 #Preview {
     ContentView()
--- Balance Eat/HomePageView.swift
+++ Balance Eat/HomePageView.swift
@@ -9,15 +9,19 @@ import SwiftUI
 
 struct HomePageView: View {
     
+    
+    
     var body: some View {
-        ZStack {
-            Color(red: 237/255, green: 237/255, blue: 237/255).edgesIgnoringSafeArea(.all)
-            
-            ScrollView{
-                NavigationStack {
+        NavigationStack {
+            ZStack {
+                Color(red: 237/255, green: 237/255, blue: 237/255).edgesIgnoringSafeArea(.all)
+                
+                ScrollView{
+                    
                     VStack {
                         SummaryView(progress: 0.7)
                             .padding(.horizontal, 16)
+                            .padding(.top, 16)
                             .toolbarBackground(.visible, for: .navigationBar)
                         
                         
@@ -25,7 +29,6 @@ struct HomePageView: View {
                         
                         TodayPlanView().padding(.horizontal, 16)
                         
-                        //                        .navigationBarTitleDisplayMode(.inline)
                         
                             .toolbar {
                                 ToolbarItem(placement: .navigationBarLeading) {
@@ -34,31 +37,39 @@ struct HomePageView: View {
                                         {
                                             // Заголовок Н6
                                             Text("Sunday, 31 dec.")
+                                            
                                                 .font(Font.custom("Lato", size: 13))
                                                 .foregroundColor(Color(red: 0.52, green: 0.52, blue: 0.52))
                                             
                                             Text("Summary")
+                                            
                                                 .font(Fonts.regular.size(28)
                                                     .weight(.semibold)
                                                 )
                                                 .foregroundColor(Color(red: 0.09,green: 0.09, blue: 0.09))
                                         }
+                                        
                                         .padding(.leading, 12)
                                     }
                                     .frame(maxWidth: .infinity)
-                                    .background(.white)
+                                    .background(.clear)
+                                    .padding(.bottom, 10)
                                 }
                                 ToolbarItem(placement: .navigationBarTrailing) {
                                     Image(systemName: "person.crop.circle")
+                                        .resizable()
+                                        .scaledToFit()
                                         .frame(width: 36, height: 36)
-                                        .background(Color(red: 0.98, green: 0.97, blue: 0.97))
+                                        .background(.clear)
+                                    
                                 }
                             }
                         
                         
                         
                     }
-                } 
+                }
+                .safeAreaPadding(.bottom,86)
             }/*.navigationTitle("Сводка").ignoresSafeArea()*/
             
         }
